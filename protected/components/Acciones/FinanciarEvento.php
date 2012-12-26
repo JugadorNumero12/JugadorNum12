e10b4cbdf5f18fcc0862555286a318c45f5a4af2
Manu <manutero@gmail.com>
2012-12-26 13:06:58 +0100

    estructura basica para las acciones; tabla xml con registro de modificadores y 2 acciones regitradas

---
+++ protected/components/Acciones/FinanciarEvento.php
@@ -0,0 +1,35 @@
+<?php
+
+/** 
+ * Descripcion breve: Financiar evento promocional
+ * Tipo: Accion grupal
+ * Perfil asociado: Empresario
+ *
+ * Efectos:
+ * 		Aforo, 		nivel medio
+ *		Ambiente,   nivel bajo
+ *
+ * Bonus al creador:
+ * 		Ninguno
+ */
+public class FinanciarEvento extends AccionSingleton
+{
+	
+   /* Mejoras para el proximo partido
+    * 	Aforo: +0.3% 
+    * 	Ambiente: +2 puntos
+    */
+   public function ejecutar($id_accion)
+   {
+   	/* TODO */
+   }
+
+   /* Restaurar valores tras el partido
+    * 	Aforo: -0.3%
+    *	Ambiente: -2 puntos
+    */
+   public function finalizar()
+   {
+   	/* TODO */
+   }	
+}
--- protected/components/Acciones/Pintarse.php
+++ protected/components/Acciones/Pintarse.php
@@ -1,33 +1,54 @@
 <?php
-/* Accion grupal */
+
+/** 
+ * Descripcion breve: Grupo de aficionados pintados con los colores del equipo
+ * Tipo: Accion grupal
+ * Perfil asociado: Ultra
+ *
+ * Efectos:
+ *    Animo,    nivel medio
+ *    Ambiente, nivel muy bajo
+ *
+ * Bonus al creador:
+ *    Animo,    nivel bajo
+ */
 public class Pintarse extends AccionSingleton
 {
 	
-   /* Todos los participantes reciben inmediatamente un extra al animo
-    *  el usuario que inicio la accion recibe un bonus
-    */
-   public function ejecutar($id_accion)
-   {
-   		$helper = new Helper();
+  /* Mejoras inmediatas para los jugadores 
+   *   Animo: +30 puntos
+   * 
+   * Mejoras para el proximo partido
+   *   Ambiente: +1 punto
+   *
+   * Bonus para el creador
+   *   Animo: +15 puntos 
+   */
+  public function ejecutar($id_accion)
+  {
+    $helper = new Helper();
 		
-   		/* Usuarios que participaron */
-   		$participantes = Participantes::model()->findbyPK($id_accion);
+ 		/* coleccion usuarios que participaron */
+ 		$participantes = Participantes::model()->findbyPK($id_accion);
 
-   		/* aumentar el animo de cada participante */
-   		foreach ($participantes as $participante)
-		{
-			$helper->aumentar_recursos($participante["id_usuario"], "animo", 30);
-		}
+ 		/* aumentar el animo de cada participante */
+ 		foreach ($participantes as $participante){
+		  $helper->aumentar_recursos($participante["id_usuario"], "animo", 30);
+	  }
 
-   		/* Usuario que la inicio */
-   		$creador = AccionesGrupales::model()->findbyPK($id_accion); //...
-   		$helper->aumentar_recursos($creador["id_usuario"], "animo", 10);
-   }
+    /* factores de partido */
+      //TODO: ambiente
+ 		
+    /* bonus del usuario que la inicio */
+ 		$creador = AccionesGrupales::model()->findbyPK($id_accion); //...
+ 		$helper->aumentar_recursos($creador["id_usuario"], "animo", 15);
+  }
 
-   /* Codigo asociado a finalizar dicha acción. P. ej.: devolver X influencias al jugador. */
-   public function finalizar()
-   {
-   
-   }	
+  /* Restaurar valores tras el partido
+   *  Ambiente: -1 punto
+   */
+  public function finalizar()
+  {
+    /* TODO */
+  }	
 }
-?>
