a40351fcaf0df558f00c6bd6c195737156e91b96
Manu <manutero@gmail.com>
2012-12-26 17:46:49 +0100

    reestructurada la tabla de efectos y esquema de las acciones

--- protected/components/Acciones/FinanciarEvento.php
+++ protected/components/Acciones/FinanciarEvento.php
@@ -5,31 +5,25 @@
  * Tipo: Accion grupal
  * Perfil asociado: Empresario
  *
- * Efectos:
- * 	 Aforo, 	nivel medio
- *	 Ambiente,  nivel bajo
+ * Efectos
+ * 	 aumenta aforo para el proximo partido 
+ *	 aumenta ambiente para el procimo partido
  *
- * Bonus al creador:
- * 	 Ninguno
+ * Bonus al creador
+ * 	 ninguno
  */
 public class FinanciarEvento extends AccionSingleton
 {
 	
-   /* Mejoras para el proximo partido
-    * 	Aforo: +0.3% 
-    * 	Ambiente: +2 puntos
-    */
-   public function ejecutar($id_accion)
-   {
-   	/* TODO */
-   }
+  /* Aplicar los efectos de la accion */
+  public function ejecutar($id_accion)
+  {
+  	/* TODO */
+  }
 
-   /* Restaurar valores tras el partido
-    * 	Aforo: -0.3%
-    *	Ambiente: -2 puntos
-    */
-   public function finalizar()
-   {
-   	/* TODO */
-   }	
+  /* Restaurar valores tras el partido */
+  public function finalizar()
+  {
+  	/* TODO */
+  }	
 }
