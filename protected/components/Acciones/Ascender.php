d368e9841bb35a7985e2202454326a011287e4a5
LtReaper <xaby693@gmail.com>
2012-12-26 20:26:17 +0100

    Completada accion de Ascender a falta de revisio para comprobar su correcto funcionamiento

--- protected/components/Acciones/Ascender.php
+++ protected/components/Acciones/Ascender.php
@@ -13,12 +13,55 @@ public class Ascender extends AccionSingleton
   /* Aplicar los efectos de la accion */
   public function ejecutar($id_accion)
   {
-  	/* TODO */
+    //Inicialmente no usamos el $id_accion
+    $trans=Yii::app()->db->beginTransaction();
+    try
+    {
+      //Aumentar generación de dinero
+      $id = Yii::app()->user->usIdent;        
+      $modelo = Recursos::model()->findByPk($id);
+      $gen_base = $modelo->getAttribute('dinero_gen');
+      $gen_mod = $gen_base + 0.12;
+      $modelo->setAttributes(array('dinero_gen'=>$gen_mod));
+      if ($modelo->save()) 
+      {
+        $trans->commit();
+      }
+      else
+      {
+        $trans->commit(); 
+      } 
+    }
+    catch (Exception $e)
+    {
+      $trans->rollBack();
+    }      
   }
 
   /* Accion permanente: metodo vacio */
   public function finalizar()
   {
-  	/* VACIO */
+    $trans=Yii::app()->db->beginTransaction();
+    try
+    {
+      //Reducir generación de dinero de nuevo
+      $id = Yii::app()->user->usIdent;        
+      $modelo = Recursos::model()->findByPk($id);
+      $gen_base = $modelo->getAttribute('dinero_gen');
+      $gen_mod = $gen_base - 0.12;
+      $modelo->setAttributes(array('dinero_gen'=>$gen_mod));
+      if ($modelo->save()) 
+      {
+        $trans->commit();
+      }
+      else
+      {
+        $trans->commit(); 
+      } 
+    }
+    catch (Exception $e)
+    {
+      $trans->rollBack();
+    }
   }	
 }
