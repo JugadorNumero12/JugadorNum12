37ab83f685ccc6e5febddc7f55b15847e9beb661
Pedro Morgado <p.morgado107@gmail.com>
2012-12-27 21:18:51 +0100

    Arreglado fallo en AccionesController

--- protected/views/acciones/ver.php
+++ protected/views/acciones/ver.php
@@ -4,15 +4,5 @@
 
 // codigo PHP
 ?>
-<<<<<<< HEAD
-=======
-<h1>Ver acción grupal</h1>
 
-<ul>
-<?php foreach ( $accionGrupal as $acciones ){
-    <li><?php echo $acciones; ?></li>
-}  ?>
-</ul>
-
->>>>>>> ad1d992e474b920d727f329f8f908e4b6d234605
 <!-- codigo HTML -->
