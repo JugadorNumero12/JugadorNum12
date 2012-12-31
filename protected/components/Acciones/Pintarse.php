c2a831a75c61564b4c673db620b85583b134a3b3
LtReaper <xaby693@gmail.com>
2012-12-31 12:07:48 +0100

    Continuada accion Pintarse a falta de ultimos retoques

--- protected/components/Acciones/Pintarse.php
+++ protected/components/Acciones/Pintarse.php
@@ -20,27 +20,56 @@ public class Pintarse extends AccionSingleton
   /* Aplica las mejoras inmediatas y para el proximo partido */
   public function ejecutar($id_accion)
   {
-    $helper = new Helper();
-		
- 		/* coleccion usuarios que participaron */
- 		$participantes = Participantes::model()->findbyPK($id_accion);
+    $trans=Yii::app()->db->beginTransaction();
+    try
+    {
+      $helper = new Helper();
+  		
+   		/* coleccion usuarios que participaron */
+   		$participantes = Participaciones::model()->findAllByAttributes(array('id_accion_grupal'=>$id_accion));
 
- 		/* aumentar el animo de cada participante */
- 		foreach ($participantes as $participante){
-		  $helper->aumentar_recursos($participante["id_usuario"], "animo", 30);
-	  }
+   		/* aumentar el animo de cada participante */
+   		foreach ($participantes as $participante)
+      {
+  		  $helper->aumentar_recursos($participante['id_usuario'], 'animo', $datos_acciones['Pintarse']['animo']);
+  	  }
 
-    /* factores de partido */
-      //TODO: ambiente
- 		
-    /* bonus del usuario que la inicio */
- 		$creador = AccionesGrupales::model()->findbyPK($id_accion); //...
- 		$helper->aumentar_recursos($creador["id_usuario"], "animo", 15);
+      /* factores de partido */
+      $fac_partido = Partidos::model()-><SELECCIONAR ENCUENTRO> 
+      $nuevo_ambiente = $fac_partido->ambiente + $datos_acciones['Pintarse']['ambiente'];
+      $fac_partido->setAttributes(array('ambiente'=>$nuevo_ambiente));   
+      $fac_partido->save();
+
+      /* bonus del usuario que la inicio */
+   		$creador = AccionesGrupales::model()->findbyPK($id_accion);
+   		$helper->aumentar_recursos($creador['id_usuario'], 'animo', $datos_acciones['Ascender']['bonus_creador']['animo']); 
+      $trans->commit();
+    }
+    catch (Exception $e)
+    {
+      $trans->rollBack();
+    } 
   }
 
   /* Restaurar valores tras el partido */
   public function finalizar()
   {
     /* TODO: solo restar el ambiente (no restar los recursos) */
+
+    //NO FIJARSE EN ESTA PARTE, ESTÁ MAL
+    $trans=Yii::app()->db->beginTransaction();
+    try
+    {
+      /* factores de partido */
+      $fac_partido = Partidos::model()-><SELECCIONAR ENCUENTRO> 
+      $nuevo_ambiente = $fac_partido->ambiente - $datos_acciones['Pintarse']['ambiente'];
+      $fac_partido->setAttributes(array('ambiente'=>$nuevo_ambiente));   
+      $fac_partido->save();
+      $trans->commit();
+    }
+    catch (Exception $e)
+    {
+      $trans->rollBack();
+    } 
   }	
 }
