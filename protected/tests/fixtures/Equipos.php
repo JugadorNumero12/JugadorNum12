8fef6edaff70452cf96d3a186e06941711e55639
Samuel <samuelmgalan@gmail.com>
2013-01-05 23:30:45 +0100

    Fixtures Equipos y Usuarios

--- protected/tests/fixtures/Equipos.php
+++ protected/tests/fixtures/Equipos.php
@@ -4,16 +4,41 @@
 /* SAM */
 // tutorial en http://www.yiiframework.com/doc/guide/1.1/en/test.fixture
 
-/* ejemplo */
 return array(
     'equipo_rojo'=>array(
         'nombre'=>'Rojos',
         'categoria'=>'1',
-        // ...
+        'aforo_max'=>'3000',
+        'aforo_base'=>'400',
+        'nivel_equipo'=>'12',
+        'factor_ofensivo'=>'7',
+        'factor_defensivo'=>'6',
     ),
     'equipo_verde'=>array(
         'nombre'=>'Verdes',
-        // ...
+        'categoria'=>'1',
+        'aforo_max'=>'3000',
+        'aforo_base'=>'500',
+        'nivel_equipo'=>'10',
+        'factor_ofensivo'=>'7',
+        'factor_defensivo'=>'6',
     ),
-    // ...
+    'equipo_negro'=>array(
+        'nombre'=>'Negros',
+        'categoria'=>'1',
+        'aforo_max'=>'3600',
+        'aforo_base'=>'400',
+        'nivel_equipo'=>'10',
+        'factor_ofensivo'=>'7',
+        'factor_defensivo'=>'7',
+    ),
+    'equipo_blanco'=>array(
+        'nombre'=>'Blancos',
+        'categoria'=>'1',
+        'aforo_max'=>'4000',
+        'aforo_base'=>'400',
+        'nivel_equipo'=>'9',
+        'factor_ofensivo'=>'6',
+        'factor_defensivo'=>'8',
+    ),
 );
