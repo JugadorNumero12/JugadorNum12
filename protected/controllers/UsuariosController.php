8de4f527da025baeb706090f399e5091b21779d6
Rober <ka2rober@gmail.com>
2013-01-07 12:16:04 +0100

    Realizada la accion RetransmitirRRSS,falta testear por no poder hacerlo de ninguna manera

--- protected/controllers/UsuariosController.php
+++ protected/controllers/UsuariosController.php
@@ -149,7 +149,7 @@ class UsuariosController extends Controller
             //Sino es correcto, mensaje de error
             if ($modelo->save()) 
             { 
-                $this->redirect(array('usuarios/cuenta'));
+               $this->redirect(array('usuarios/cuenta'));
             }
            
         }
