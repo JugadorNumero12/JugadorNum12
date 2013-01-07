d60bfe67a35e6a40a146b4ffe1625880e58cb469
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2013-01-07 14:08:24 +0100

    Modificaciones en la vista index de Partidos.

--- protected/views/partidos/index.php
+++ protected/views/partidos/index.php
@@ -1,16 +1,19 @@
 <?php
-/* @var ejemplo de variable dada por el controlador */
-/* @var ejemplo de variable dada por el controlador */
-
-// codigo PHP
-
-?>
-
-<?php 
+/* @var $esDeUsuario indica si el equipo del usuario juega en cada partido */
+/* @var $equiposL contiene los equipos locales de cada partido */
+/* @var $equiposL contiene los equipos visitantes de cada partido */
+ 
 	$count = count($equiposL); //count($equiposL) = count($equiposV) por construccion
 	for ($i = 0; $i < $count; $i++) {
-			$local     = $equiposL[$i];
-			$visitante = $equiposV[$i];
-			echo $local['nombre'].' vs '.$visitante['nombre'].'</br>';		
+
+			//creamos un string con el nombre de los 2 equipos concatenados con un 'vs'
+			$local      = $equiposL[$i];
+			$visitante  = $equiposV[$i];
+			$strPartido = $local['nombre'].' vs '.$visitante['nombre'].'</br>';
+
+			//ponemos en negrita los partidos en los que juega el equipo del usuario
+			if($esDeUsuario[$i]) 
+				 echo '<b>'.$strPartido.'</b>';
+			else echo $strPartido;		
 	} 
 ?>
