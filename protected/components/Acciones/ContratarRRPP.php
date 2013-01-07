cb17251b484de7bf5b8c49444f5f0d016bb5dacc
Marina <marinabezares@gmail.com>
2013-01-07 18:42:22 +0100

    funcin ejecutar de la acccion ContratarRRPP hecha

--- protected/components/Acciones/ContratarRRPP.php
+++ protected/components/Acciones/ContratarRRPP.php
@@ -11,9 +11,41 @@
 public class ContratarRRPP extends AccionSingleton
 {
 	/* Aplicar los efectos de la accion  */
-	public void ejecutar()
+	public void ejecutar($bonus)
 	{
-		/* TODO */
+		//Voy a modificar la base de datos asi que comienzo una tranasacción
+		$trans=Yii::app()->db->beginTransaction();
+	    try
+	    {
+	    	//Consigo el id de usuarios y los datos de sus recursos
+	      $idUsuario = Yii::app()->user->usIdent;        
+	      $modeloRecursos = $idUsuario->recursos;
+	      //Si da fallo salimos del if (y acabamos el try)
+	      if($modeloRecursos == null)
+	      {
+	      	break;
+	      }
+	      else
+	      {
+	      	//Si no conseguimos actualizamos los bonus actuales. 
+	      	//Como esta habilidad nos hace gastar menos influencia restamos el bonus 
+	      	$bonusActuales=$recursos->bonus_influencias;
+			$valor_nuevo=$bonusActuales - $bonus;
+
+			/*Si save() no lanza error entonces se realizo correctamente la actualizacion
+			sino salimos el if (y acabamos el try)*/
+			if($recursos->save())
+			{
+				break;
+			}
+
+	      }
+	      $trans->commit();
+	    }
+	    catch (Exception $e)
+	    {
+	      $trans->rollBack();
+	    }     
 	}
 
 	/* Restarurar valores tras el partido */
