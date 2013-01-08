440ee8ef1dbc2f1ed8b2ed51b6a264fed01a1f88
Pedro Morgado <p.morgado107@gmail.com>
2013-01-08 16:40:42 +0100

    Accion OrganizarEvento

--- protected/components/Acciones/OrganizarHomenaje.php
+++ protected/components/Acciones/OrganizarHomenaje.php
@@ -16,12 +16,34 @@ public class OrganizarHomenaje extends AccionSingleton
 	/* Aplicar los efectos de la accion */
 	public void ejecutar()
 	{
-		/* TODO */
+		$trans = Yii::app()->db->beginTransaction();
+
+      	try{
+        	$helper = new Helper();
+        
+        	//Busco el siguiente partido (Falta seleccionar el partido actual)
+        	$partido = Partidos::model()-><SELECCIONAR ENCUENTRO>;
+        	
+        	//Saco el nuevo aforo y se lo sumo al que había
+        	$nuevoAforo = $partido->aforo + $datos_acciones['OrganizarHomenaje']['aforo'];
+        	$partido->setAttributes(array('aforo'=>$nuevoAforo)); 
+
+        	$partido->save(); 
+
+        	//Bonus al creador: le aumento las influencias
+        	$creador = AccionesGrupales::model()->findbyPK($id_accion);
+   			$helper->aumentar_recursos($creador['id_usuario'], 'influencias_max',
+   					$datos_acciones['OrganizarHomenaje']['bonus_creador']['influencias_max']); 
+
+        	$trans->commit();
+      } catch {
+        	$trans->rollBack();
+      }
 	}
 
 	/* Restarurar valores tras el partido */
 	public void finalizar()
 	{
-		/* TODO */
+		//
 	}
 }
