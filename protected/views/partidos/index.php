54a3101c72f3228eb879c0b3e2eac47cead2aff8
Marina <marinabezares@gmail.com>
2013-01-08 13:41:44 +0100

    mas cambios en vistas

--- protected/views/partidos/index.php
+++ protected/views/partidos/index.php
@@ -5,23 +5,6 @@
 /* @var $equiposL contiene los equipos visitantes de cada partido */
 /* @var $idPartidos los id de los partidos en los que participa el equipo del usuario */
  
-/*	$count = count($equiposL); //count($equiposL) = count($equiposV) por construccion
-	for ($i = 0; $i < $count; $i++) {
-
-			//creamos un string con el nombre de los 2 equipos concatenados con un 'vs'
-			$local      = $equiposL[$i];
-			$visitante  = $equiposV[$i];
-			$strPartido = $local['nombre'].' vs '.$visitante['nombre'].'</br>'; 
-
-			if($esDeUsuario[$i]) 
-			{ 
-				 echo '<b>'.$strPartido.'</b>';
-			}
-			else
-			{ 
-				echo $strPartido;		
-			}
-	} */
 ?>
 
 
@@ -42,12 +25,18 @@
 
 							<?php if($esDeUsuario[$i]) 
 									{ 
-										 echo '<b>'.$strPartido.'</b>';
+										echo '<b>'.$strPartido.'</b>';
 									}
 									else
 									{ 
 										echo $strPartido;		
 									} ?></td>
+							</a>
+		<td align="center"> <a href=
+									"<?php echo $this->createUrl( '/partidos/asistir', 
+									array('id_partido' => $idPartidos[$i]) ); ?>">  
+
+							 		<input type="button" value="Asistir"/> </td>
 							</a>
 	</tr>
 	<?php } ?>
