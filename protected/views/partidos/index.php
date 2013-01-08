b6a07508d3658d5b683fe804c36ab757887493fb
Marina <marinabezares@gmail.com>
2013-01-08 13:19:49 +0100

    cambiada vista partidos/index

--- protected/views/partidos/index.php
+++ protected/views/partidos/index.php
@@ -1,19 +1,55 @@
+
 <?php
 /* @var $esDeUsuario indica si el equipo del usuario juega en cada partido */
 /* @var $equiposL contiene los equipos locales de cada partido */
 /* @var $equiposL contiene los equipos visitantes de cada partido */
+/* @var $idPartidos los id de los partidos en los que participa el equipo del usuario */
  
-	$count = count($equiposL); //count($equiposL) = count($equiposV) por construccion
+/*	$count = count($equiposL); //count($equiposL) = count($equiposV) por construccion
 	for ($i = 0; $i < $count; $i++) {
 
 			//creamos un string con el nombre de los 2 equipos concatenados con un 'vs'
 			$local      = $equiposL[$i];
 			$visitante  = $equiposV[$i];
-			$strPartido = $local['nombre'].' vs '.$visitante['nombre'].'</br>';
+			$strPartido = $local['nombre'].' vs '.$visitante['nombre'].'</br>'; 
 
-			//ponemos en negrita los partidos en los que juega el equipo del usuario
 			if($esDeUsuario[$i]) 
+			{ 
 				 echo '<b>'.$strPartido.'</b>';
-			else echo $strPartido;		
-	} 
+			}
+			else
+			{ 
+				echo $strPartido;		
+			}
+	} */
 ?>
+
+
+<table cellspacing="5px">
+	<?php 
+	$count = count($equiposL);
+	for ($i = 0; $i < $count; $i++) { 
+
+		//creamos un string con el nombre de los 2 equipos concatenados con un 'vs'
+		$local      = $equiposL[$i];
+		$visitante  = $equiposV[$i];
+		$strPartido = $local['nombre'].' vs '.$visitante['nombre'].'</br>'; 
+		?>
+	<tr>
+		<td align="center"> <a href=
+									"<?php echo $this->createUrl( '/partidos/previa', 
+									array('id_partido' => $idPartidos[$i]) ); ?>">  
+
+							<?php if($esDeUsuario[$i]) 
+									{ 
+										 echo '<b>'.$strPartido.'</b>';
+									}
+									else
+									{ 
+										echo $strPartido;		
+									} ?></td>
+							</a>
+	</tr>
+	<?php } ?>
+	<tr></tr>
+</table>
