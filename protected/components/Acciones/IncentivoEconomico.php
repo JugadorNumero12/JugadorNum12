567021d22a2595be3a58cb11d3d71716f150879c
Samuel <samuelmgalan@gmail.com>
2013-01-14 11:27:30 +0100

    Dos fixtures que faltaban y acciones/IncentivoEconomico

--- protected/components/Acciones/IncentivoEconomico.php
+++ protected/components/Acciones/IncentivoEconomico.php
@@ -17,12 +17,52 @@ public class IncentivoEconomico extends AccionSingleton
 	/* Aplicar los efectos de la accion */
 	public void ejecutar()
 	{
-		/* TODO */
+		/* SAM */
+		 $trans=Yii::app()->db->beginTransaction();
+		 try {
+		 	$helper = new Helper();
+
+		 	//Aumentar nivel_equipo
+        	$id_partido = Partidos::model()-><SELECCIONAR ENCUENTRO>;
+        	$id_equipo = Yii::app()->user->usAfic;
+        	$columna = 'nivel_equipo';
+        	//La cantidad no creo que sea esta
+        	$cantidad = $datos_acciones['IncentivoEconomico']['nivel_equipo'];
+        	$helper->aumentar_factores($id_partido,$id_equipo,$columna,$cantidad);
+
+        	//Bonus creador
+        	$creador = AccionesGrupales::model()->findByPk($id_equipo);
+        	//La cantidad no creo que sea esta
+        	$cantidad = $datos_acciones['IncentivoEconomico']['bonus_creador']['influencias'];
+        	$helper->aumentar_recursos($creador['id_usuario'],'influencias',$cantidad);
+
+		 	$trans->commit();
+		 	
+		 } catch (Exception $e) {
+		 	$trans->rollBack();
+		 }
 	}
 
 	/* restarurar valores tras el partido */
 	public void finalizar()
 	{
-		/* TODO */
+		/* SAM */
+		$trans=Yii::app()->db->beginTransaction();
+		 try {
+		 	$helper = new Helper();
+
+		 	//Aumentar nivel_equipo
+        	$id_partido = Partidos::model()-><SELECCIONAR ENCUENTRO>;
+        	$id_equipo = Yii::app()->user->usAfic;
+        	$columna = 'nivel_equipo';
+        	//La cantidad no creo que sea esta
+        	$cantidad = $datos_acciones['IncentivoEconomico']['nivel_equipo'];
+        	$helper->disminuir_factores($id_partido,$id_equipo,$columna,$cantidad);
+
+		 	$trans->commit();
+		 	
+		 } catch (Exception $e) {
+		 	$trans->rollBack();
+		 }
 	}
 }
