a4ae37f13d74750690953368d9a38ca1952cc9f3
Alexandra <leximagination@gmail.com>
2013-01-15 23:51:15 +0100

    habilidades_ver

--- protected/views/habilidades/ver.php
+++ protected/views/habilidades/ver.php
@@ -8,37 +8,44 @@
 
 ?>
 
-<div class="envoltorio-ver-habilidad">
+<div class="envoltorio-todo">
 
-<h1>Habilidad: <?php echo $habilidad['nombre']; ?></h1>
+	<h1>Habilidad: <?php echo $habilidad['nombre']; ?></h1>
+	<div class = "tabla">
+		<table>
+			<tr>
+				<td>Descripci&oacute;n: </td>
+				<td><?php echo $habilidad['descripcion']; ?></td>
+			</tr>
+			<tr>
+				<td>Tipo habilidad: </td>
+				<td><?php echo ($habilidad['tipo']==Habilidades::TIPO_INDIVIDUAL) ? 'Individual' : 'Grupal' ?></td>
+			</tr>
+			<tr>
+				<td>Coste: </td>
+			</tr>
+			<tr>
+				<td>Dinero: </td>
+				<td><?php echo $habilidad['dinero']; ?></td>
+			</tr>
+			<tr>
+				<td>&Aacute;nimo: </td>
+				<td><?php echo $habilidad['animo']; ?></td>
+			</tr>
+			<tr>
+				<td>Influencia: </td>
+				<td><?php echo $habilidad['influencias']; ?></td>
+			</tr>
+		</table>
+	</div>
 
-<table>
-	<tr>
-		<td>Descripci&oacute;n: </td>
-		<td><?php echo $habilidad['descripcion']; ?></td>
-	</tr>
-	<tr>
-		<td>Tipo habilidad: </td>
-		<td><?php echo ($habilidad['tipo']==Habilidades::TIPO_INDIVIDUAL) ? 'Individual' : 'Grupal' ?></td>
-	</tr>
-	<tr>
-		<td>Coste: </td>
-	</tr>
-	<tr>
-		<td>Dinero: </td>
-		<td><?php echo $habilidad['dinero']; ?></td>
-	</tr>
-	<tr>
-		<td>&Aacute;nimo: </td>
-		<td><?php echo $habilidad['animo']; ?></td>
-	</tr>
-	<tr>
-		<td>Influencia: </td>
-		<td><?php echo $habilidad['influencias']; ?></td>
-	</tr>
-
-</table>
-<a href="<?php echo $this->createUrl('/habilidades/index'); ?>">&larr; Habilidades</a>
+	<div class = "boton">
+	 <tr>
+	    <td >
+	    	<?php echo CHtml::submitButton('Habilidades',array('submit' => array('/habilidades/index'),'class'=>"button verysmall black"));?>
+	    </td>
+	  </tr>
+	</div>
 </div>
 
 
