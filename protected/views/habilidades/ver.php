9651aa41f0bc2026e7df08160e88e5f370dd4abb
Alexandra <leximagination@gmail.com>
2013-01-21 13:53:05 +0100

    ver_habilidad 0.1

--- protected/views/habilidades/ver.php
+++ protected/views/habilidades/ver.php
@@ -2,62 +2,44 @@
 /* @var ejemplo de variable dada por el controlador */
 /* @var ejemplo de variable dada por el controlador */
 
-// codigo PHP
+/*
+
+			<td>Coste: </td>
+				<td>Dinero: </td>
+				<td><?php echo $habilidad['dinero']; ?></td>
+				<td>&Aacute;nimo: </td>
+				<td><?php echo $habilidad['animo']; ?></td>
+				<td>Influencia: </td>
+				<td><?php echo $habilidad['influencias']; ?></td>
+*/
 
 ?>
 
-<div class="envoltorio-todo">
+<div class="envoltorio-ver-habilidad">
 
-	<h1>Habilidad: <?php echo $habilidad['nombre']; ?></h1>
-	<div class = "tabla">
-		<table>
-			<tr>
-				<td>Descripci&oacute;n: </td>
-				<td><?php echo $habilidad['descripcion']; ?></td>
-			</tr>
-			<tr>
-				<td>Tipo habilidad: </td>
-				<td><?php switch ($habilidad['tipo']){
+	<h1><?php echo 'Habilidad: '; echo $habilidad['nombre']; ?></h1>
+
+	<div class="separador">
+		<?php echo 'Descripción: '; echo $habilidad['descripcion']; ?>
+	</div>
+
+<div class="separador">
+			<?php echo 'Tipo habilidad: '; switch ($habilidad['tipo']){
 					case Habilidades::TIPO_INDIVIDUAL:
-						echo 'Individual';
+						echo 'Acción individual.';
 					  break;
 					case Habilidades::TIPO_GRUPAL:
-					  	echo 'Grupal';
+					  	echo 'Acción grupal.';
 					  break;
 					case Habilidades::TIPO_PARTIDO:
-					  	echo 'De Partido';
+					  	echo 'Acción de partido.';
 					  break;
 					case Habilidades::TIPO_PASIVA:
-					  	echo 'Pasiva';
+					  	echo 'Acción pasiva.';
 					  break;
-				} ?></td>
-			</tr>
-			<tr>
-				<td>Coste: </td>
-			</tr>
-			<tr>
-				<td>Dinero: </td>
-				<td><?php echo $habilidad['dinero']; ?></td>
-			</tr>
-			<tr>
-				<td>&Aacute;nimo: </td>
-				<td><?php echo $habilidad['animo']; ?></td>
-			</tr>
-			<tr>
-				<td>Influencia: </td>
-				<td><?php echo $habilidad['influencias']; ?></td>
-			</tr>
-		</table>
-	</div>
-
-	<div class = "boton">
-	 <tr>
-	    <td >
-	    	<?php echo CHtml::submitButton('Habilidades',array('submit' => array('/habilidades/index'),'class'=>"button verysmall black"));?>
-	    </td>
-	  </tr>
-	</div>
+				}
+		?></div>
+<div class='separador'>
 </div>
 
-
-<!-- codigo HTML -->
+</div>
