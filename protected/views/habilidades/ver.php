e5c1f2f5b679222403bbc8cc7f7e162bd0b5f79c
Alexandra <leximagination@gmail.com>
2013-01-21 18:02:09 +0100

    tabla y tildes

--- protected/views/habilidades/ver.php
+++ protected/views/habilidades/ver.php
@@ -5,63 +5,63 @@
 ?>
 
 <div class="envoltorio-ver-habilidad">
+	<table class = 'tabla-ver-habilidad'>
+		<tr>
+			<h1><?php echo $habilidad['nombre']; ?></h1>
+		</tr>
 
-	<h1><?php echo $habilidad['nombre']; ?></h1>
+		<div class = 'separador'><tr>
+			<?php echo '<b>Descripci&oacute;n: </b>'; echo $habilidad['descripcion']; ?>
+		</tr></div>
 
-	<div class="separador">
-		<?php echo '<b>Descripción: </b>'; echo $habilidad['descripcion']; ?>
-	</div>
-
-	<div class="separador">
-		<?php echo '<b>Tipo habilidad: </b>'; 
+		<div class = 'separador'><tr>
+			<?php echo '<b>Tipo habilidad: </b>'; 
 			switch ($habilidad['tipo']){
 				case Habilidades::TIPO_INDIVIDUAL:
-					echo 'Acción individual.';
+					echo 'Acci&oacute;n individual.';
 				  break;
 				case Habilidades::TIPO_GRUPAL:
-				  	echo 'Acción grupal.';
+				  	echo 'Acci&oacute;n grupal.';
 				  break;
 				case Habilidades::TIPO_PARTIDO:
-				  	echo 'Acción de partido.';
+				  	echo 'Acci&oacute;n de partido.';
 				  break;
 				case Habilidades::TIPO_PASIVA:
-				  	echo 'Acción pasiva.';
+				  	echo 'Acci&oacute;n pasiva.';
 				  break;
 			}
-		?>
-	</div>
+			?>
+		</tr></div>
 
-	<div class="separador">
-		<?php echo '<b>Recursos necesarios para activar la habilidad: </b>'?>
-		<?php printf('<b>Dinero: </b>%d <b>Animo</b>: %d <b>Influencias: </b>%d', $habilidad['dinero'], $habilidad['animo'], $habilidad['influencias']); ?>
-	</div>
-
-
-
-	<div class ="separador">
-		<?php 
-		if($habilidad['tipo'] == Habilidades::TIPO_GRUPAL){
-			printf('<b>Número máximo de participantes: </b>%d',$habilidad['participantes_max']);?>
-	</div>	
+		<div class="separador"><tr>
+			<?php echo '<b>Recursos necesarios para activar la habilidad: </b>'?>
+			<?php printf('<b>Dinero: </b>%d <b>&Aacute;nimo</b>: %d <b>Influencias: </b>%d', $habilidad['dinero'], $habilidad['animo'], $habilidad['influencias']); ?>
+		</tr></div>
 
 
+		<div class ="separador"><tr>
+			<?php 
+				if($habilidad['tipo'] == Habilidades::TIPO_GRUPAL){
+					printf('<b>N&uacute;mero m&aacute;ximo de participantes: </b>%d',$habilidad['participantes_max']);
+			?>
+		</tr></div>	
 
-	<div class ="separador">
-			<?php echo '<b>Valores máximos que los recursos pueden sumar: </b>';
-			printf('<b>Dinero: </b>%d <b>Animo</b>: %d <b>Influencias: </b>%d', $habilidad['dinero_max'], $habilidad['animo_max'], $habilidad['influencias_max']);
-		}
-		?>
-	</div>
+		<div class ="separador"><tr>
+			<?php echo '<b>Valores m&aacute;ximos que los recursos pueden sumar: </b>';
+			printf('<b>Dinero: </b>%d <b>&Aacute;nimo</b>: %d <b>Influencias: </b>%d', $habilidad['dinero_max'], $habilidad['animo_max'], $habilidad['influencias_max']);
+			}
+			?>
+		</tr></div>
 
+		<div class="separador"><tr>
+			<?php
+				if($habilidad['tipo'] == Habilidades::TIPO_INDIVIDUAL || 
+					$habilidad['tipo'] == Habilidades::TIPO_PARTIDO || 
+					$habilidad['tipo'] == Habilidades::TIPO_GRUPAL){
+					printf('<b>Cooldown: </b>%d',$habilidad['cooldown_fin']);
+				}
+			?>
+		</tr></div>
 
-
-	<div class="separador">
-		<?php
-		if($habilidad['tipo'] == Habilidades::TIPO_INDIVIDUAL || 
-			$habilidad['tipo'] == Habilidades::TIPO_PARTIDO || 
-			$habilidad['tipo'] == Habilidades::TIPO_GRUPAL){
-			printf('<b>Cooldown: </b>%d',$habilidad['cooldown_fin']);
-		}
-	?>
-	</div>
+	</table>
 </div>
