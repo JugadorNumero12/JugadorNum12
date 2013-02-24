0018d38308a65b603567ff89d7724f1e3ed2fb9e
Xaby <ltreap.face@gmail.com>
2013-02-24 12:24:11 +0100

    Completado codigo general de la funcion finalizar para acciones individuales. Comprueba parametros y devuelve influencias al jugador.

--- protected/components/Acciones/AccionIndSingleton.php
+++ protected/components/Acciones/AccionIndSingleton.php
@@ -27,7 +27,27 @@ class AccionIndSingleton
    public function ejecutar($id_usuario){ }
 
    /* Codigo asociado a finalizar dicha acción. P. ej.: devolver X influencias al jugador. */
-   public function finalizar($id_usuario){ }
+   public function finalizar($id_usuario,$id_habilidad)
+   {
+      //Validar usuario
+      $us = Usuarios::model()->findByPk($id_usuario);
+      if ($us == null)
+         throw new Exception("Usuario incorrecto.", 404); 
+
+      //Tomar y validar habilidad
+      $hab = Habilidades::model()->findByPk($id_habilidad); 
+      if ($hab == null)
+         throw new Exception("Habilidad incorrecta.", 404); 
+      //Coger influencias a devolver
+      $influencias = $hab->influencias;   
+
+      //Tomar helper para facilitar la modificación
+      Yii::import('application.components.Helper');
+
+      //Aumentar ánimo
+      $helper = new Helper();
+      ($helper->aumentar_recursos($id_usuario,"influencias",$influencias) == 0)? return 0 : return -1;
+   }
 
    /* Evita que el objeto se pueda clonar */
    public function __clone()
