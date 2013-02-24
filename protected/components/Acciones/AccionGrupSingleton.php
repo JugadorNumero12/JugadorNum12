f034e01e21954f6927c15ed088287221d8524684
Xaby <ltreap.face@gmail.com>
2013-02-24 13:17:35 +0100

    Creado singleton para acciones grupales

---
+++ protected/components/Acciones/AccionGrupSingleton.php
@@ -0,0 +1,39 @@
+<?php 
+class AccionGrupSingleton
+{
+   /* Instancia del objeto */
+   private static $instancia;   
+
+   /* Incluir tabla de efectos */
+   include('tabla_efectos.php');
+
+   /* Constructora privada para evitar instanciación externa */
+   private function __construct()
+   {
+      echo "Creado singletonDeAccionGrupal"; //Eliminar!!
+   }
+
+   /* Función a través de la cual se accederá al Singleton */
+   public static function getInstance()
+   {
+      if (!self::$instancia instanceof self)
+      {
+         self::$instancia = new self;
+      }
+      return self::$instancia;
+   }
+
+   /* Codigo asociado a ejecutar dicha acción. P. ej.: dar X de ánimo al jugador. 
+   Tambien devuelve las influencias aportadas.
+   Se llamará al crear una grupal o participar en ella, siempre y cuando los recursos hayan sido alcanzados. */
+   public function ejecutar($id_accion){ }
+
+   public function finalizar() { }
+
+   /* Evita que el objeto se pueda clonar */
+   public function __clone()
+   {
+      trigger_error('Clonación no permitida.', E_USER_ERROR);
+   } 
+}
+?>
