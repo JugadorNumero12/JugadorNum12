eceb3dec03b2a5dca45100a7ffb37b08a70b19da
Alexandra <leximagination@gmail.com>
2013-02-26 14:47:30 +0100

    Recursos Script

--- protected/commands/TestCommand.php
+++ protected/commands/TestCommand.php
@@ -2,9 +2,15 @@
 class TestCommand extends CConsoleCommand {
     public function run($args) {
         // here we are doing what we need to do
+        $trans = Yii::app()->db->beginTransaction();
+		try{
         $mod = Usuarios:: model()->findByPk(9);
                 $mod->nivel += 3;
                 $mod->save();
+                $trans->commit();
+        catch(Exception $e){
+			$trans->rollback();
+        }
     }
 }
 ?>
