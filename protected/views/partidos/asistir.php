fbfe033d7566d6ab9d526052ef212ce35a5a4870
Xaby <ltreap.face@gmail.com>
2013-02-27 10:25:26 +0100

    Comentada la vista asistir.php y creado el div necesario para el renderPartial por Ajax

--- protected/views/partidos/asistir.php
+++ protected/views/partidos/asistir.php
@@ -1,11 +1,22 @@
 <?php
-/* @var $equipo_local el equipo local */
-/* @var $equipo_visitante el equipo visitante */
-/* @var $partido el partido a mostrar */
+/* PONER VARIABLES DEL SCRIPT */
+
+/*
+*
+* ESTRUCTURA DE LA VISTA
+* 1.- Un <div> que servirá para contener todas las secciones 1, 2, 3 y 4. Es 
+* necesario porque Ajax lo necesita para saber qué meterá dentro. 
+* IMPORTANTE: no es necesario meter ahora dentro de ese <div> las secciones
+* correspondientes ya que será el renderPartial (de la vista _estadoPartido)
+* el que las meta ahí. Solo hace falta que ese <div> tenga el tamaño y posición
+* adecuados para contenerlas.
+* IMPORTANTE: este div es el que hay abajo creado. Lo único que falta es darle un 
+* ID o clase con CSS para tamaños y todo eso. No hay que meter nada más en él salvo
+* la llamada que ya tiene dentro (no borrarla).
+* 1.- Un <div> bajo este contenedor anterior para meter las acciones de partido.
+*/
 ?>
 
-<h3> 
-	Has asistido al partido entre <i> <?php echo $equipo_local->nombre ?> </i>
-	y <i> <?php echo $equipo_visitante->nombre ?> </i> </br>
-	Coming soon.
-</h3>
+<div>
+	<?php $this->renderPartial('_estadoPartido',array('estado' => $partido)); ?>
+</div>
