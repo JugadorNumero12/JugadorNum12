5890aa391d92682be5c16a116f3fb68a825c7979
Pedro Morgado <p.morgado107@gmail.com>
2013-02-27 16:37:00 +0100

    Cambiado archivo de scripts para el main

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $("img.escudos-clasificacion").hover(function(){
-    var texto = $("img.escudos-clasificacion").val();
-	$("img.escudos-clasificacion").show(texto);
-  });
-});
