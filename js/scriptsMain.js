608688bb9f3761960a61f260bfedc7b7312baef9
Samuel <samuelmgalan@gmail.com>
2013-02-28 20:22:31 +0100

    Aniadidas barras de progreso al menu lateral

--- js/scriptsMain.js
+++ js/scriptsMain.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
     var texto = $("img.escudos-clasificacion").val();
 	$("img.escudos-clasificacion").show(texto);
   });
+
+  	var maxAnimo = parseInt(document.getElementById("bar").getAttribute("data-max"));
+	$("td#bar").progressbar({max: maxAnimo});
+	var valorAnimo = parseInt(document.getElementById("bar").getAttribute("data-valor"));
+	$("td#bar").progressbar({value: valorAnimo});
+
+	var maxInflu = parseInt(document.getElementById("bar2").getAttribute("data-max"));
+	$("td#bar2").progressbar({max: maxInflu});
+	var valorInflu = parseInt(document.getElementById("bar2").getAttribute("data-valor"));
+	$("td#bar2").progressbar({value: valorInflu});
+
+	var label1 = $(".label1");
+	label1.text($("td#bar").progressbar("value") + "/"+ $("td#bar").progressbar("option", "max"));
+	var label2 = $(".label2");
+	label2.text($("td#bar2").progressbar("value") + "/"+ $("td#bar2").progressbar("option", "max"));
 });
