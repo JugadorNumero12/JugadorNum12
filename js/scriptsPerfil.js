ad5375a60e3a531435cf9992b367bc32ed3d1a9d
Samuel <samuelmgalan@gmail.com>
2013-02-28 19:43:03 +0100

    Barras de progreso mejoradas en Perfil

--- js/scriptsPerfil.js
+++ js/scriptsPerfil.js
@@ -8,4 +8,9 @@ $(document).ready(function(){
 	$("td#bar2").progressbar({max: maxInflu});
 	var valorInflu = parseInt(document.getElementById("bar2").getAttribute("data-valor"));
 	$("td#bar2").progressbar({value: valorInflu});
+
+	var label1 = $(".label1");
+	label1.text($("td#bar").progressbar("value") + "/"+ $("td#bar").progressbar("option", "max"));
+	var label2 = $(".label2");
+	label2.text($("td#bar2").progressbar("value") + "/"+ $("td#bar2").progressbar("option", "max"));
 });
--- protected/views/usuarios/perfil.php
+++ protected/views/usuarios/perfil.php
@@ -68,8 +68,8 @@
 						<tr><th>Nivel: </th> <td><?php echo $modeloU->nivel ?> </td> </tr> 
 						<tr><th> <br></th> <td> </td> <br></tr> 
 						<tr><th>Dinero: </th> <td><?php echo $modeloU->recursos->dinero ?></td> </tr> 
-						<tr><th>&Aacute;nimo: </th> <td class="barrita" id="bar" data-valor="<?php echo $modeloU->recursos->animo?>" data-max="<?php echo $recursos->animo_max ?>"></td> </tr> 
-						<tr><th>Influencias: </th> <td class="barrita" id="bar2" data-valor="<?php echo $modeloU->recursos->influencias?>" data-max="<?php echo $recursos->influencias_max ?>"></td> </tr> 
+						<tr><th>&Aacute;nimo: </th> <td class="barrita" id="bar" data-valor="<?php echo $modeloU->recursos->animo?>" data-max="<?php echo $recursos->animo_max ?>"><div class="label1">Label</div></td> </tr> 
+						<tr><th>Influencias: </th> <td class="barrita" id="bar2" data-valor="<?php echo $modeloU->recursos->influencias?>" data-max="<?php echo $recursos->influencias_max ?>"><div class="label2">Label</div></td> </tr> 
 						<tr><th> <br></th> <td> </td> <br></tr> 
 						<tr><th>Email: </th><td><?php echo $modeloU->email ?></td></tr>
 						<tr><td><?php echo CHtml::submitButton('Cambiar contraseña', array('submit' => array('cambiarClave'),'class'=>"button small black"));?></td>
