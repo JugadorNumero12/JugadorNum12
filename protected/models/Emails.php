7acf12a0320b5ee5fffbac4a50e7791985bb80f8
Alexandra <leximagination@gmail.com>
2013-03-06 22:06:15 +0100

    Active Record for Emails

---
+++ protected/models/Emails.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Modelo de la tabla <<emails>>
+ *
+ * Columnas disponibles
+ * string 	$id_email
+ * string 	$id_usuario_to
+ * string 	$id_usuario_from
+ * string 	$fecha
+ * string 	$contenido
+ * string	$leido
+ * string 	$asunto
+ */
+class Emails extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Emails the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'emails';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{ 	
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('id_usuario_to, id_usuario_from, fecha, contenido, leido, asunto', 'required'),
+			array('asunto', 'length', 'max'=>50),
+			array('id_email, id_usuario_to, id_usuario_from', 'length', 'max'=>10),
+			array('leido', 'length', 'max'=>1),
+			array('fecha', 'length', 'max'=>19),
+
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id_email, id_usuario_to, id_usuario_from, fecha, contenido, leido, asunto', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * Define las relaciones entre <emails - tabla>
+	 *
+	 * @devuelve array de relaciones
+	 */
+	public function relations()
+	{
+		return array(
+			/*Relacion entre <<emails>> y <<usuarios>>*/
+			'usuarioTo'=>array(self::BELONGS_TO, 'Usuarios', 'id_usuario_to'),
+			/*Relacion entre <<emails>> y <<usuarios>>*/
+			'usuarioFor'=>array(self::BELONGS_TO, 'Usuarios', 'id_usuario_from'),
+		 );
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id_email' => 'Id Email',
+			'id_usuario_to' => 'Id Usuario Para',
+			'id_usuario_from' => 'Id Usuario De',
+			'fecha' => 'Fecha',
+			'contenido' => 'Contenido',
+			'leido' => 'Leido',
+			'asunto' => 'Asunto',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id_email',$this->id_email,true);
+		$criteria->compare('id_usuario_to',$this->id_usuario_to,true);
+		$criteria->compare('id_usuario_from',$this->id_usuario_from,true);
+		$criteria->compare('fecha',$this->fecha,true);
+		$criteria->compare('contenido',$this->contenido,true);
+		$criteria->compare('leido',$this->leido);
+		$criteria->compare('asunto',$this->asunto,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
