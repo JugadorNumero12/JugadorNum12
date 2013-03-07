a636e68c956f749b6396a6bcdc0eb9c9fa9257cc
Marina <marinabezares@gmail.com>
2013-03-07 20:19:56 +0100

    arreglada un poquillo vista email index

--- protected/views/emails/index.php
+++ protected/views/emails/index.php
@@ -16,14 +16,27 @@
 			    ));
  ?>
 
-<h3>boton redactar</h3> <br>
-<h3>boton enviados</h3> <br>
-<?php foreach ( $emails as $i=>$email ){ 
-	//en funcion de si se ha leido o no que muestre un color diferente o lo que sea
-	echo $niks[$i];
-	echo $email->asunto;
-	echo Yii::app()->dateFormatter->formatDateTime($email->fecha, 'medium', 'short');
-	echo CHtml::button('Leer', array('submit' => array('emails/leerEmail', 'id'=>$email->id_email),'class'=>"button small black"));
-}?>
+
+<table>
+	<tr> <th>Enviado por </th> <th> Asunto </th> <th> Fecha </th> <th> Leido </th> <th> &nbsp; </th> </tr>
+
+<?php foreach ( $emails as $i=>$email ){ ?>
+	
+	<tr> 
+		<td> <?php echo $niks[$i]; ?> </td> 
+		<td> <?php echo $email->asunto; ?> </td> 
+		<td> <?php echo Yii::app()->dateFormatter->formatDateTime($email->fecha, 'medium', 'short'); ?> </td> 
+		<td> <?php if ($email->leido == 0) {echo 'No';} else {echo 'Sí';}?> </td> 
+		<td> <?php echo CHtml::button('Leer', array('submit' => array('emails/leerEmail', 'id'=>$email->id_email),'class'=>"button small black")); ?> </td> 
+	</tr>
+		
+<?php }?>
+
+</table>
+
+<br> <br>
+<?php echo CHtml::button('Redactar mensaje', array('submit' => array('emails/redactar'),'class'=>"button small black")); ?> <br> <br>
+<?php echo CHtml::button('Enviados', array('submit' => array('emails/enviados'),'class'=>"button small black")); ?> <br> <br>
+
 
  <?php $this->endWidget(); ?>
