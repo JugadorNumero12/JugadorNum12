5d3d267680449c726d843bb745afd646d1df2879
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2013-03-07 13:05:57 +0100

    Arreglados links muertos de imágenes en la vista usuarios/ver.

--- protected/views/usuarios/ver.php
+++ protected/views/usuarios/ver.php
@@ -16,13 +16,13 @@
 				<?php switch ($modeloU->personaje)
 								{
 								case Usuarios::PERSONAJE_ULTRA: ?>
-								  <img src="<?php echo Yii::app()->BaseUrl.'/less/imagenes/perfil/ultra.jpg'; ?>" width=300 height=300 border=0 alt="Ultra"> 
+								  <img src="<?php echo Yii::app()->BaseUrl.'/images/perfil/ultra.jpg'; ?>" width=300 height=300 border=0 alt="Ultra"> 
 								  <?php break;
 								case Usuarios::PERSONAJE_MOVEDORA:?>
-								  <img src="<?php echo Yii::app()->BaseUrl.'/less/imagenes/perfil/animadora.jpg'; ?>" width=300 height=300 border=0 alt="Animadora"> 
+								  <img src="<?php echo Yii::app()->BaseUrl.'/images/perfil/animadora.jpg'; ?>" width=300 height=300 border=0 alt="Animadora"> 
 								  <?php break;
 								case Usuarios::PERSONAJE_EMPRESARIO:?>
-								  <img src="<?php echo Yii::app()->BaseUrl.'/less/imagenes/perfil/empresario.jpg'; ?>" width=300 height=300 border=0 alt="Empresario"> 
+								  <img src="<?php echo Yii::app()->BaseUrl.'/images/perfil/empresario.jpg'; ?>" width=300 height=300 border=0 alt="Empresario"> 
 								  <?php break;
 								} ?>
 				</div>
@@ -32,16 +32,16 @@
 				<?php switch ($modeloU->equipos->id_equipo)
 								{
 								case 1: ?>
-								  <img src="<?php echo Yii::app()->BaseUrl.'/less/imagenes/escudos/escudo-rojo.png'; ?>" width=100 height=100 border=0 alt="Escudo rojo"> 
+								  <img src="<?php echo Yii::app()->BaseUrl.'/images/escudos/escudo-rojo.png'; ?>" width=100 height=100 border=0 alt="Escudo rojo"> 
 								  <?php break;
 								case 2:?>
-								  <img src="<?php echo Yii::app()->BaseUrl.'/less/imagenes/escudos/escudo-verde.png'; ?>" width=100 height=100 border=0 alt="Escudo verde"> 
+								  <img src="<?php echo Yii::app()->BaseUrl.'/images/escudos/escudo-verde.png'; ?>" width=100 height=100 border=0 alt="Escudo verde"> 
 								  <?php break;
 								case 3:?>
-								  <img src="<?php echo Yii::app()->BaseUrl.'/less/imagenes/escudos/escudo-negro.png'; ?>" width=100 height=100 border=0 alt="Escudo negro"> 
+								  <img src="<?php echo Yii::app()->BaseUrl.'/images/escudos/escudo-negro.png'; ?>" width=100 height=100 border=0 alt="Escudo negro"> 
 								  <?php break;
 								  case 4:?>
-								  <img src="<?php echo Yii::app()->BaseUrl.'/less/imagenes/escudos/escudo-blanco.png'; ?>" width=100 height=100 border=0 alt="Escudo blanco"> 
+								  <img src="<?php echo Yii::app()->BaseUrl.'/images/escudos/escudo-blanco.png'; ?>" width=100 height=100 border=0 alt="Escudo blanco"> 
 								  <?php break;
 								} ?>
 				</div>
