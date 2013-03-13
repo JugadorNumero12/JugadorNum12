8adac90ddaf71d112ceb4b66802d75c59a48f687
Daniel Escoz <darkhogg@gmail.com>
2013-03-13 10:44:00 +0100

    Reestructurado el menú por completo.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -67,22 +67,24 @@
 			</div>
 
 			<div id="barrasup-derecha">
-				<ul id="barrasup-perfil">
-					<li class="barrasup-menu">
+				<ul id="user-menu">
+					<li class="user-menu-item">
 						<img alt="<?php echo Yii::app()->getParams()->usuario->nick; ?>"
-						     src="" width="26" height="26">
-						<?php echo Yii::app()->getParams()->usuario->nick; ?>
+						     src="" width="24" height="24"><span class="user-menu-txt"><?php echo Yii::app()->getParams()->usuario->nick; ?></span>
 					</li>
-					<li class="barrasup-submenu"><ul>
-						<a href="<?php echo Yii::app()->createUrl('/usuarios/perfil') ?>"><li>
-							<img alt="Perfil" src="<?php echo Yii::app()->BaseUrl ?>/images/iconos/menu/barra-perfil.png" width="24" height="24"/>
-							Perfil
-						</li></a>
-						<a href="<?php echo Yii::app()->createUrl('/site/logout') ?>"><li>
-							<img alt="Perfil" src="<?php echo Yii::app()->BaseUrl ?>/images/iconos/menu/barra-logout.png" width="24" height="24"/>
-							Logout
-						</li></a>
-					</ul></li>
+
+					<a href="<?php echo Yii::app()->createUrl('/usuarios/perfil') ?>">
+						<li class="user-menu-item user-menu-hidden">
+							<img alt="Perfil" src="<?php echo Yii::app()->BaseUrl ?>/images/iconos/menu/barra-perfil.png"
+							     width="24" height="24"/><span class="user-menu-txt">Perfil</span>
+						</li>
+					</a>
+					<a href="<?php echo Yii::app()->createUrl('/site/logout') ?>">
+						<li class="user-menu-item user-menu-hidden">
+							<img alt="Logout" src="<?php echo Yii::app()->BaseUrl ?>/images/iconos/menu/barra-logout.png"
+							     width="24" height="24"/><span class="user-menu-txt">Logout</span>
+						</li>
+					</a>
 				</ul>
 			</div>
 		</div>
