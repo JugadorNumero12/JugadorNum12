b47e3032def1a6f7aa426deb0415c7ab0bf45a05
Alexandra <leximagination@gmail.com>
2013-03-17 20:04:27 +0100

    cambio vista notificaciones

--- js/notificaciones.js
+++ js/notificaciones.js
@@ -1,6 +1,8 @@
 (function($){
 	$(document).ready(function(){
 		
+		$.jGrowl.defaults.position = 'center';
+		
 		$.jGrowl.defaults.closer = false;
 
 		$.jGrowl.defaults.animateOpen = {
@@ -10,22 +12,6 @@
 			width: 'hide'
 		};
 
-		$.jGrowl.defaults.position = 'center';
-
-		<?php if($aux == "algo" && $accionGrupal['completada'] == 1){ ?>
-			$.jGrowl("¡Enhorabuena, has completado la acción¡", { sticky: true });
-		<?php } ?>
-
-		<?php if ($accionGrupal['completada'] == 1 && $aux != "algo"){ ?>
-			$.jGrowl("La acción se ha completado", { sticky: true });
-		<?php } ?>
-
-		<?php if($aux == "nada" && $accionGrupal['completada'] != 1){ ?>
-			$.jGrowl("No has aportado nada a la acción.", { sticky: true });
-		<?php } ?>
-
-		<?php if($aux == "algo" && $accionGrupal['completada'] != 1){ ?>
-			$.jGrowl("Tu equipo agradece tu generosa contribución.", { sticky: true });
-		<?php } ?>
 	});
+
 })(jQuery);
