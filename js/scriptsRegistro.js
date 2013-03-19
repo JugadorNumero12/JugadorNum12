18361f4bd7d76e7c0fef212d76b70cda13ee0035
Arturo Pareja <arturo.pareja.garcia@gmail.com>
2013-03-19 09:34:33 +0100

    Arreglado el estilo de registro de personajes. Añadida una función JQuery para seleccionar personaje.

--- js/scriptsRegistro.js
+++ js/scriptsRegistro.js
@@ -3,3 +3,9 @@ $.fn.setIdEquipo = function(val) {
 	$(".escudo-seleccion").removeClass("escudo-seleccion");
     $(this).addClass("escudo-seleccion");
 };
+
+$.fn.setIdPersonaje = function(val) {
+    $('#'+val)[0].checked = true; 
+	$(".pj-seleccion").removeClass("pj-seleccion");
+    $(this).addClass("pj-seleccion");
+};
--- protected/views/registro/personaje.php
+++ protected/views/registro/personaje.php
@@ -2,6 +2,27 @@
 
 ?>
 
+<div class="envoltorio-elegir-personaje">
+
+<div class="contenido-elegir-personaje">
+	<h3>ELIJA UN PERSONAJE</h3>
+	<div class="descripcion-personaje">Pulse en la imagen de su personaje y haga click en finalizar. </div>
+
+<div class="elegir-personaje">
+	<table>		
+		<tr>
+			<td><input type="image" value="radio-ultra" 	 onclick="$(this).setIdPersonaje(this.value);" title="Lorem ipsum dolor sit amet, consectetur adipiscing elit." src="<?php echo Yii::app()->BaseUrl.'/images/perfil/ultra.jpg'; ?>" class="personaje"></td>
+			<td><input type="image" value="radio-animadora"  onclick="$(this).setIdPersonaje(this.value);" title="Lorem ipsum dolor sit amet, consectetur adipiscing elit." src="<?php echo Yii::app()->BaseUrl.'/images/perfil/animadora.jpg'; ?>" class="personaje"></td>
+			<td><input type="image" value="radio-empresario" onclick="$(this).setIdPersonaje(this.value);" title="Lorem ipsum dolor sit amet, consectetur adipiscing elit." src="<?php echo Yii::app()->BaseUrl.'/images/perfil/empresario.jpg'; ?>" class="personaje"></td>
+		</tr>
+		<tr>
+			<td>Ultra</td>
+			<td>Animadora</td>
+			<td>Empresario</td>			
+		</tr>
+	</table>
+</div>
+
 <?php
 	$form = $this->beginWidget('CActiveForm', array(
 				'id'=>'usuarios-form',
@@ -10,43 +31,27 @@
 			    'clientOptions'=>array(
 					'validateOnSubmit'=>true,),
 			    ));
- ?>
-
-<div class="elegir-personaje">
+?>
+<div class="campos-ocultos">
 	<table>
 		<tr>
-			<td>Animadora</td>
-			<td>Empresario</td>
-			<td>Ultra</td>
-		</tr>
-		<tr>
-			<td><img title="Lorem ipsum dolor sit amet, consectetur adipiscing elit." src="<?php echo Yii::app()->BaseUrl.'/images/perfil/animadora.jpg'; ?>" class="personaje"></td>
-			<td><img title="Lorem ipsum dolor sit amet, consectetur adipiscing elit." src="<?php echo Yii::app()->BaseUrl.'/images/perfil/empresario.jpg'; ?>" class="personaje"></td>
-			<td><img title="Lorem ipsum dolor sit amet, consectetur adipiscing elit." src="<?php echo Yii::app()->BaseUrl.'/images/perfil/ultra.jpg'; ?>" class="personaje"></td>
-		</tr>
-
-
-		<tr>
-	  	<td>
-	  		<?php echo CHtml::radioButton('pers', $animadora_status,array('value'=>'animadora','uncheckValue'=>null)); ?>
-	  	</td>
-	  	<td>
-	  		<?php echo CHtml::radioButton('pers', $empresario_status,array('value'=>'empresario','uncheckValue'=>null)); ?>
-	  	</td>
-	  	<td>
-	  		<?php echo CHtml::radioButton('pers', $ultra_status,array('value'=>'ultra','uncheckValue'=>null)); ?>
-	  	</td>
+	 		<td>
+	  		<?php echo CHtml::radioButton('pers', $ultra_status,
+	  									  array('id'=>'radio-ultra','value'=>'ultra','uncheckValue'=>null)); ?>
+	  		</td>
+	  		<td>
+	  		<?php echo CHtml::radioButton('pers', $animadora_status,
+	  									  array('id'=>'radio-animadora','value'=>'animadora','uncheckValue'=>null)); ?>
+	  		</td>
+	  		<td>
+	  		<?php echo CHtml::radioButton('pers', $empresario_status,
+	  									   array('id'=>'radio-empresario','value'=>'empresario','uncheckValue'=>null)); ?>
+	  		</td>
 	  	</tr>
-
-
-
-		<!-- <tr><?php if($error): ?>
-	   	<td>¡Error!</td>
-	  	<?php endif; ?></tr> -->
-
 	</table>
-	<br>
-	<div><?php echo CHtml::submitButton('Finalizar',array('class'=>"button large black")); ?></div>
 </div>
+<div><?php echo CHtml::submitButton('Finalizar',array('class'=>"button large black")); ?></div>
 
-<?php $this->endWidget(); ?>
+</div>
+	<?php $this->endWidget(); ?>
+</div>
