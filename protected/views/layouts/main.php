6256aa8a53a4baa1e575b3f02b68cd058836b4c3
Daniel Escoz <darkhogg@gmail.com>
2013-03-19 13:14:34 +0100

    Resuelto conflicto y añadidas cosillas del menú

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -83,19 +83,9 @@
 			<ul id="user-menu">
 				<!-- Avatar + Nombre -->
 				<li class="user-menu-item">
-					<?php switch (Yii::app()->getParams()->usuario->personaje){
-							case Usuarios::PERSONAJE_EMPRESARIO: ?>
-								<img alt="<?php echo Yii::app()->getParams()->usuario->nick; ?>" src="<?php echo Yii::app()->createUrl('/images/perfil/empresario.jpg') ?>" width="24" height="24">
-								<?php break;
-							case Usuarios::PERSONAJE_MOVEDORA: ?>
-								<img alt="<?php echo Yii::app()->getParams()->usuario->nick; ?>" src="<?php echo Yii::app()->createUrl('/images/perfil/animadora.jpg') ?>" width="24" height="24">
-								<?php break;
-							case Usuarios::PERSONAJE_ULTRA: ?>
-								<img alt="<?php echo Yii::app()->getParams()->usuario->nick; ?>" src="<?php echo Yii::app()->createUrl('/images/perfil/ultra.jpg') ?>" width="24" height="24">
-								<?php break;
-									
-					} ?>
-					<span class="user-menu-txt user-menu-title"><?php echo Yii::app()->getParams()->usuario->nick; ?></span>
+					<img alt="<?php echo Yii::app()->getParams()->usuario->nick; ?>"
+					     src="<?php echo Yii::app()->createUrl('/images/perfil/animadora.jpg') ?>"
+					     width="24" height="24"><span class="user-menu-txt user-menu-title"><?php echo Yii::app()->getParams()->usuario->nick; ?></span>
 				</li>
 
 				<!-- Link al perfil -->
@@ -121,7 +111,7 @@
 <div id="envoltorio-main">
 
   	<!-- DIVISION DE CABECERA -->
-	<div id="clasificacion">
+	<div id="clasificacion" class="top-block">
         <ul>
 		<?php foreach (Yii::app()->getParams()->clasificacion as $equipo): ?>
             <a href="<?php echo $this->createUrl( '/equipos/ver', array('id_equipo' => $equipo->equipos_id_equipo) ); ?>">  
@@ -139,59 +129,55 @@
 	</div>
 	
     <!-- DIVISION DEL MENU IZQUIERDO -->
-    <div id="menu-izquierdo">
-		<div id='cssmenu'>
-			<ul>
-				<a href="<?php echo $this->createUrl( '/usuarios/index', array() ); ?>">
-					<li class="elementos-menu">
-						<?php switch (Yii::app()->getParams()->usuario->personaje){
-							case Usuarios::PERSONAJE_EMPRESARIO: ?>
-								<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/perfil-empresario-2.png'; ?>" alt="menu-inicio-empresario">
-								<?php break;
-							case Usuarios::PERSONAJE_MOVEDORA: ?>
-								<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/perfil-RRPP-2.png'; ?>" alt="menu-inicio-movedora">
-								<?php break;
-							case Usuarios::PERSONAJE_ULTRA: ?>
-								<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/perfil-ultra.png'; ?>" alt="menu-inicio-ultra">
-								<?php break;
-									
-						} ?>
-						<div class="nombre-menu">Inicio</div>
-					</li>
-				</a>
-				<a href="<?php echo $this->createUrl( '/equipos/ver', array('id_equipo' => Yii::app()->user->usAfic) ); ?>">
-				   	<li class="elementos-menu">
-						<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/aficion.png'; ?>" alt="menu-aficion">
-						<div class="nombre-menu">Afici&oacute;n</div>
-					</li>
-				</a>
-				<a href="<?php echo Yii::app()->createUrl('/habilidades');?>">
-				   	<li class="elementos-menu">
-				   		<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/habilidades.png'; ?>" alt="menu-arbol">
-				   		<div class="nombre-menu">Habilidades</div>
-				   	</li>
-			    </a>
-			   	<a href="<?php echo Yii::app()->createUrl('/acciones');?>">
-			   		<li class="elementos-menu">
-			   			<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/habilidades/habilidades.png'; ?>" alt="menu-desbloqueadas">
-			   			<div class="nombre-menu">&Aacute;rbol</div>
-			   		</li>
-			   	</a>			   	
-				<a href="<?php echo Yii::app()->createUrl('/partidos/index');?>">
-				   	<li class="elementos-menu">
-				   		<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/calendario.png'; ?>" alt="menu-calendario">
-				   		<div class="nombre-menu">Calendario</div>
-				   	</li>
-			  	</a>
-			    <a href="<?php echo Yii::app()->createUrl('/equipos');?>">
-				   	<li class="elementos-menu">
-				   		<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/clasificacion.png'; ?>" alt="menu-clasificacion">
-				   		<div class="nombre-menu">Clasificaci&oacute;n</div>
-				   	</li>
-				</a>
-			</ul>
-		</div>
-    </div>
+    <ul id="menu-izquierdo" class="top-block">
+		<a href="<?php echo $this->createUrl( '/usuarios/index', array() ); ?>">
+			<li class="menu-item menu-item-first">
+				<?php switch (Yii::app()->getParams()->usuario->equipos_id_equipo){
+					case Usuarios::PERSONAJE_EMPRESARIO: ?>
+						<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/perfil-empresario-2.png'; ?>" alt="menu-inicio-empresario">
+						<?php break;
+					case Usuarios::PERSONAJE_MOVEDORA: ?>
+						<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/perfil-RRPP-2.png'; ?>" alt="menu-inicio-movedora">
+						<?php break;
+					case Usuarios::PERSONAJE_ULTRA: ?>
+						<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/perfil-ultra.png'; ?>" alt="menu-inicio-ultra">
+						<?php break;
+							
+				} ?>
+				<div class="nombre-menu">Inicio</div>
+			</li>
+		</a>
+		<a href="<?php echo $this->createUrl( '/equipos/ver', array('id_equipo' => Yii::app()->user->usAfic) ); ?>">
+		   	<li class="menu-item">
+				<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/aficion.png'; ?>" alt="manu-aficion">
+				<div class="nombre-menu">Afici&oacute;n</div>
+			</li>
+		</a>
+		<a href="<?php echo Yii::app()->createUrl('/habilidades');?>">
+		   	<li class="menu-item">
+		   		<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/habilidades/habilidades.png'; ?>" alt="perfil-sin-definir">
+		   		<div class="nombre-menu">Habilidades</div>
+		   	</li>
+	    </a>
+	   	<a href="<?php echo Yii::app()->createUrl('/acciones');?>">
+	   		<li class="menu-item">
+	   			<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/perfil-sin-definir.png'; ?>" alt="perfil-sin-definir">
+	   			<div class="nombre-menu">Desbloqueadas</div>
+	   		</li>
+	   	</a>			   	
+		<a href="<?php echo Yii::app()->createUrl('/partidos/index');?>">
+		   	<li class="menu-item">
+		   		<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/tabla.png'; ?>" alt="menu-calendario">
+		   		<div class="nombre-menu">Calendario</div>
+		   	</li>
+	  	</a>
+	    <a href="<?php echo Yii::app()->createUrl('/equipos');?>">
+		   	<li class="menu-item menu-item-last">
+		   		<img class="icono-menu" src="<?php echo Yii::app()->BaseUrl.'/images/iconos/menu/clasificacion.png'; ?>" alt="menu-clasificacion">
+		   		<div class="nombre-menu">Clasificaci&oacute;n</div>
+		   	</li>
+		</a>
+	</ul>
     
     <!-- DIVISION PARA FLOTAR -->
     <div id="grupo-centro">
