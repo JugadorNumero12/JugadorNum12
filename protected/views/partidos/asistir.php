0810d7fccdb9bd9a401d31a09c3e714622cdf461
Daniel Escoz <darkhogg@gmail.com>
2013-04-07 23:58:54 +0200

    Añadidos datos reales a la vista de partido.
    Issue #79

--- protected/views/partidos/asistir.php
+++ protected/views/partidos/asistir.php
@@ -6,26 +6,28 @@
 <div id="partido-marcador">
 	<!-- Información general -->
 	<div id="partido-marcador-general">
-		<div id="partido-goles">0 - 1</div>
+		<div id="partido-goles"><?php echo $partido->goles_local ?> - <?php echo $partido->goles_visitante ?></div>
 		<div id="partido-tiempo">12:34</div>
 		<div id="partido-tiempo-turno">01:23</div>
-		<div id="partido-ambiente">Caldeado</div>
+		<div id="partido-ambiente"><?php echo $partido->ambiente ?></div>
 	</div>
 
 	<!-- Información del equipo local -->
 	<div id="partido-equipo-local">
-		<div id="partido-nombre-local">Localitos</div>
-		<div id="partido-escudo-local">Escudo local aquí</div>
-		<div id="partido-nivel-local">Nivel L</div>
-		<div id="partido-aforo-local">123</div>
+		<div id="partido-nombre-local"><?php echo $eqLoc->nombre ?></div>
+		<div id="partido-escudo-local"><img src="<?php
+			echo Yii::app()->BaseUrl . '/images/escudos/' . $eqLoc->token . '.png' ?>" /></div>
+		<div id="partido-nivel-local">Nivel <?php echo $eqLoc->nivel_equipo ?></div>
+		<div id="partido-aforo-local"><?php echo $partido->aforo_local ?> asistentes</div>
 	</div>
 
 	<!-- Información del equipo visitante -->
 	<div id="partido-equipo-visit">
-		<div id="partido-nombre-visit">Visititos</div>
-		<div id="partido-escudo-visit">Escudo visit aquí</div>
-		<div id="partido-nivel-visit">Nivel V</div>
-		<div id="partido-aforo-visit">321</div>
+		<div id="partido-nombre-visit"><?php echo $eqVis->nombre ?></div>
+		<div id="partido-escudo-visit"><img src="<?php
+			echo Yii::app()->BaseUrl . '/images/escudos/' . $eqVis->token . '.png' ?>" /></div>
+		<div id="partido-nivel-visit">Nivel <?php echo $eqVis->nivel_equipo ?></div>
+		<div id="partido-aforo-visit"><?php echo $partido->aforo_visitante ?> asistentes</div>
 	</div>
 </div>
 
