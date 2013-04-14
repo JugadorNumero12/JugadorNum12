4af065af932116711ca508af393f80271ce4c392
Marina <marinabezares@gmail.com>
2013-04-14 15:19:06 +0200

    arhora para desbloquear las habilidades necesitas desbloquear otras antes

--- protected/components/Acciones/RequisitosDesbloquearHabilidades.php
+++ protected/components/Acciones/RequisitosDesbloquearHabilidades.php
@@ -4,6 +4,7 @@
  *
  * listado de claves esperables
  *		nivel=>valor
+  * 	desbloqueadas_previas => array(claves esperables)
  */
 class RequisitosDesbloquearHabilidades
 {
@@ -12,65 +13,80 @@ class RequisitosDesbloquearHabilidades
 		// GRUPALES
 		// ------------------------------------------------
 		'FinanciarEvento' => array (
-			'nivel'=> 0,
+			'nivel'=> 5,
+			'desbloqueadas_previas' => array ('IncentivoEconomico' => 'IncentivoEconomico',
+												'Apostar' => 'Apostar'),
 		), 
 			
 		'IncentivoEconomico' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 		
 		'OrganizarHomenaje' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 			
 		'Pintarse' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 		
 		'PromoverPartido' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 			
 		// INDIVIDUALES
 		// ------------------------------------------------
 		'Apostar' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 		
 		'CrearseEspectativas' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		),
 			
 		'ContratarRRPP' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		),
 		
 		//PARTIDO 
 		// ------------------------------------------------
 		'BeberCerveza' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 		
 		'HablarSpeaker' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 			
 		'IniciarOla' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 		
 		'PunteroLaser' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 		
 		'RetransmitirRRSS' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		),
 
 		// PASIVAS
 		// -------------------------------------------------
 		'Ascender' => array (
 			'nivel' => 5,
+			'desbloqueadas_previas' => array (),
 		), 
 	);
 }
