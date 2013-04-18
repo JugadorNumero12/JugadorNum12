ed259bdba30ff8fd2ea265cd7ed4b6677a11e79e
Marina <marinabezares@gmail.com>
2013-04-18 12:17:20 +0200

    commit para hacer pull

--- protected/views/notificaciones/index.php
+++ protected/views/notificaciones/index.php
@@ -1,13 +1,13 @@
-<div class="envoltorio">
 <div class="encabezado"> <h1>Mis Notificaciones</h1> </div>
 
 <?php 
 foreach ( $notificaciones as $notificacion ){ ?>
     
-    <div class="descripcion-accion"> 
-        <?php echo $notificacion['mensaje'];?> 
-        <?php echo Yii::app()->dateFormatter->formatDateTime($notificacion['fecha'], 'medium', 'short'); ?>
-        <?php echo CHtml::button('Leer', array('submit' => array('notificaciones/leer', 'id'=>$notificacion['id_notificacion'], 'url'=>$notificacion['url']),'class'=>"button small black"));?>
+    <div class="lista-notificaciones"> 
+    	<li>
+        	<?php echo $notificacion['mensaje'];?> 
+        	<?php echo Yii::app()->dateFormatter->formatDateTime($notificacion['fecha'], 'medium', 'short'); ?>
+       	 	<?php echo CHtml::button('Leer', array('submit' => array('notificaciones/leer', 'id'=>$notificacion['id_notificacion'], 'url'=>$notificacion['url']),'class'=>"button small black"));?>
+    	</li>
     </div>    
  <?php } ?>
-</div>
