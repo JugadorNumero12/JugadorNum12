55a34018a3ae50704a3ed0c6f9171594837e3f44
Marina <marinabezares@gmail.com>
2013-04-20 21:27:56 +0200

    ahora cuando soy el unico participante de una accion no se guarda notificacion

--- protected/models/AccionesGrupales.php
+++ protected/models/AccionesGrupales.php
@@ -483,22 +483,29 @@ class AccionesGrupales extends CActiveRecord
             Yii::app()->user->setFlash('completada', '¡Enhorabuena, has completado la acción¡');
         } else {
         	//si se ha aportado algo creamos notivicación
-			$notificacion = new Notificaciones;
-			$notificacion->fecha = time();
-			$notificacion->mensaje = Usuarios::model()->findByPk($id_user)->nick . " ha participado en la acción " . Habilidades::model()->findByPk($habilidad->id_habilidad)->nombre;
-			$notificacion->url = "acciones/ver?id_accion=". $id_accion;
-            $notificacion->imagen = "images/iconos/participacion_grupal.png";
-			$notificacion->save();
-			//enviamos notificaciones a los participantes de la acción
-			$participaciones = Participaciones::model()->findAllByAttributes(array('acciones_grupales_id_accion_grupal'=>$id_accion));
-			foreach($participaciones as $participacion){
-				if($participacion->usuarios_id_usuario != $id_user){
-					$usrnotif = new Usrnotif;
-					$usrnotif->notificaciones_id_notificacion = $notificacion->id_notificacion;
-					$usrnotif->usuarios_id_usuario = $participacion->usuarios_id_usuario;//$habilidad->id_habilidad;//
-					$usrnotif->save();
-				}
-			}
+            $participaciones = Participaciones::model()->findAllByAttributes(array('acciones_grupales_id_accion_grupal'=>$id_accion));
+
+            //Si soy el unico en participar no creamos la notificacion
+            if(count($participaciones) > 1 ){
+
+    			$notificacion = new Notificaciones;
+    			$notificacion->fecha = time();
+    			$notificacion->mensaje = Usuarios::model()->findByPk($id_user)->nick . " ha participado en la acción " . Habilidades::model()->findByPk($habilidad->id_habilidad)->nombre;
+    			$notificacion->url = "acciones/ver?id_accion=". $id_accion;
+                $notificacion->imagen = "images/iconos/participacion_grupal.png";
+    			$notificacion->save();
+    			//enviamos notificaciones a los participantes de la acción
+    			
+    			foreach($participaciones as $participacion){
+    				if($participacion->usuarios_id_usuario != $id_user){
+    					$usrnotif = new Usrnotif;
+    					$usrnotif->notificaciones_id_notificacion = $notificacion->id_notificacion;
+    					$usrnotif->usuarios_id_usuario = $participacion->usuarios_id_usuario;//$habilidad->id_habilidad;//
+    					$usrnotif->save();
+    				}
+    			}
+            }
+
             Yii::app()->user->setFlash('aporte', 'Tu equipo agradece tu generosa contribución.');
         }
     }
