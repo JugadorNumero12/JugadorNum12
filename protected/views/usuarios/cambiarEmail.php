bee4e908e0baeab37d30757f5092341ce5a6c2a6
Xaby <ltreap.face@gmail.com>
2013-04-21 13:02:14 +0200

    Agregado boton de vuelta en los formularios de cambio de datos

--- protected/views/usuarios/cambiarEmail.php
+++ protected/views/usuarios/cambiarEmail.php
@@ -16,30 +16,31 @@
 
 <div id="form-cambio">
 	<table cellspacing="5px">
-		  <tr>
-		    <td align="center"><span class="cambio">CAMBIO DE EMAIL</span></td>
-		  </tr>
-		  <tr>
-		    <td align="center"><?php echo $form->emailField($model,'antigua_email', array('placeholder' => 'Email actual', 'size' => 50)); ?></td>
-		  </tr>
-		  <tr>
-		    <td><?php echo $form->error($model,'antigua_email'); ?></td>
-		  </tr>
-		  <tr>
-		    <td align="center"><?php echo $form->emailField($model,'nueva_email1', array('placeholder' => 'Nuevo email', 'size' => 50)); ?></td>
-		  </tr>
-		  <tr>
-		    <td><?php echo $form->error($model,'nueva_email1'); ?></td>
-		  </tr>
-		  <tr>
-		    <td align="center"><?php echo $form->emailField($model,'nueva_email2', array('placeholder' => 'Repita nuevo email', 'size' => 50)); ?></td>
-		  </tr>
-		  <tr>
-		    <td><?php echo $form->error($model,'nueva_email2'); ?></td>
-		  </tr>
-		  <tr>
-		    <td align="center"><?php echo CHtml::submitButton('Cambiar email');?></td>
-		  </tr>
+	  <tr>
+	    <td align="center" colspan="2"><span class="cambio">CAMBIO DE EMAIL</span></td>
+	  </tr>
+	  <tr>
+	    <td align="center" colspan="2"><?php echo $form->emailField($model,'antigua_email', array('placeholder' => 'Email actual', 'size' => 50)); ?></td>
+	  </tr>
+	  <tr>
+	    <td colspan="2"><?php echo $form->error($model,'antigua_email'); ?></td>
+	  </tr>
+	  <tr>
+	    <td align="center" colspan="2"><?php echo $form->emailField($model,'nueva_email1', array('placeholder' => 'Nuevo email', 'size' => 50)); ?></td>
+	  </tr>
+	  <tr>
+	    <td colspan="2"><?php echo $form->error($model,'nueva_email1'); ?></td>
+	  </tr>
+	  <tr>
+	    <td align="center" colspan="2"><?php echo $form->emailField($model,'nueva_email2', array('placeholder' => 'Repita nuevo email', 'size' => 50)); ?></td>
+	  </tr>
+	  <tr>
+	    <td colspan="2"><?php echo $form->error($model,'nueva_email2'); ?></td>
+	  </tr>
+	  <tr>
+    	<td align="center"><?php echo CHtml::button('Volver',array('onclick' => 'js:document.location.href="'.$this->createUrl('usuarios/perfil').'"','class'=>"button large black"));?></td>
+	    <td align="center"><?php echo CHtml::submitButton('Cambiar email');?></td>
+	  </tr>
 	</table>
 </div>
 <?php $this->endWidget(); ?>
