7b6ee74b495e36f76088f0fecfa25e8ccfa7325c
Marina <marinabezares@gmail.com>
2013-04-23 18:16:55 +0200

    commit tonto porque se me olvido comentar todo antes

--- protected/views/notificaciones/index.php
+++ protected/views/notificaciones/index.php
@@ -10,7 +10,7 @@ foreach ( $notificaciones as $notificacion ){ ?>
         	<div class="contenido-notificaciones">
         		<p><?php echo $notificacion['mensaje'];?> </p>
         		<div class="fecha-notificaciones"><?php echo Yii::app()->dateFormatter->formatDateTime($notificacion['fecha'], 'medium', 'short'); ?></div>
-       	 		<div class="boton-notificaciones"><?php echo CHtml::button('Leer', array('submit' => array('notificaciones/leer', 'id'=>$notificacion['id_notificacion'], 'url'=>$notificacion['url']),'class'=>"button small black"));?></div>
+       	 		<!--<div class="boton-notificaciones"><?//php echo CHtml::button('Leer', array('submit' => array('notificaciones/leer', 'id'=>$notificacion['id_notificacion'], 'url'=>$notificacion['url']),'class'=>"button small black"));?></div> -->
        	 	</div>
     	</li>
     </div>    
