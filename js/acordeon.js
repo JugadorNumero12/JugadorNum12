bca8eef14e9d8d1c285b245ef863c0f3e238986e
Pedro Morgado <p.morgado107@gmail.com>
2013-04-24 12:29:52 +0200

    Ajustado el acordeon

--- js/acordeon.js
+++ js/acordeon.js
@@ -1,11 +1,7 @@
 $(function() {
     $( ".accordion" ).accordion({
    		collapsible: true,
-<<<<<<< HEAD
    		autoHeight: false,
    		active: false
-=======
-   		autoHeight: false
->>>>>>> aff4995d93aab49bac65caf3f5941b376d7ce4d6
     });
 });
