86026a47264494ad52f69308ce1b2153c6c15671
Rober <ka2rober@gmail.com>
2013-05-16 12:44:26 +0200

    realizadas ,acciones ,de, partido, quedan, poner, los, efectos

--- protected/components/Acciones/DoblarApuesta.php
+++ protected/components/Acciones/DoblarApuesta.php
@@ -38,6 +38,23 @@ class DoblarApuesta extends AccionPartSingleton
 	public function ejecutar($id_usuario,$id_partido,$id_equipo)
 	{
       // TODO
+      $ret =0 ; 
+      //Traer el array de efectos
+      parent::ejecutar($id_usuario,$id_partido,$id_equipo);
+
+      //Validar usuario
+      $us = Usuarios::model()->findByPk($id_usuario);
+      if ($us === null)
+        throw new Exception("Usuario incorrecto.", 404); 
+      // Cojo el equipo del usuario
+      $equipo = $us->equipos;
+      // Modifico Los factores de ese partido
+      $ret = min($ret,Partidos::aumentar_factores($id_partido,$equipo->id_equipo,"moral",Efectos::$datos_acciones['HablarSpeaker']['moral']));
+      
+      // Incorporo un registro a la tabla acciones turno si el usuario aun no esta en ella
+      AccionesTurno::incorporarAccion($id_usuario, $id_partido,$id_equipo);
+     //Finalizar función
+      return $ret;
 	}
 
  	/**
